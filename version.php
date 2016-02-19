d9b5637831425010bbbecafb016c5ec87d413fa9
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2016-02-19 11:42:35 +1100

    Bumped internal version for testing.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016021903;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016021904;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015050500;        // Requires this Moodle version.
 $plugin->component = 'local_qrlinks';   // Full name of the plugin (used for diagnostics).
